d5e791c91cc52a80334d8bc2c911be81194ab8ed
627km <rudals_jeon@naver.com>
2023-01-26 16:41:07 +0900

    feat: Add order_detail page and comment
    Add order_detail page
    Add comment list
    Add create comment

--- shinhantoy/order/views.py
+++ shinhantoy/order/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework import mixins, generics
-from .models import Order
-from .serializers import OrderSerializer
+from .models import Order, Comment
+from .serializers import OrderSerializer, CommentSerializer, CommentCreateSerializer
 from .paginations import OrderPagination
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class OrderListView(
@@ -18,4 +19,39 @@ class OrderListView(
     def get(self, request, *args, **kwargs):
         return self.list(request, args, kwargs)
     
+class OrderDetailView(
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView,
+):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+
+        return Order.objects.filter(pk=pk)
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, args, kwargs)
+
+class CommentListView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView,
+):
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return CommentCreateSerializer
+        return CommentSerializer
+
+    def get_queryset(self):
+       pk = self.kwargs.get('pk')
+       return Comment.objects.filter(order_id = pk)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, args, kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, args, kwargs)
+
+
     
